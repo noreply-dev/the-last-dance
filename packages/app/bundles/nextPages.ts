505585e8d9023a0225dc0e5a379decfe4a9d71f7
cristian-sanchez-dev <cristian.sanchez@protofy.xyz>
2023-10-08 21:16:11 +0000

    [FT](Web)! Integrating UI in the bundles

--- packages/app/bundles/nextPages.ts
+++ packages/app/bundles/nextPages.ts
@@ -1,13 +1,15 @@
 import notesPages from './notes/nextPages'
+import workspacePages from './workspace/nextPages'
 import databasePages from 'protolib/adminpanel/bundles/database/adminPages'
 import devicePages from 'protolib/adminpanel/bundles/devices/adminPages'
 import filesPages from 'protolib/adminpanel/bundles/files/adminPages'
 import usersPages from 'protolib/adminpanel/bundles/users/adminPages'
 
 export default {
-    ...notesPages,
-    ...databasePages,
-    ...devicePages,
-    ...filesPages,
-    ...usersPages
+  ...notesPages,
+  ...workspacePages,
+  ...databasePages,
+  ...devicePages,
+  ...filesPages,
+  ...usersPages
 }
