cb258c0787433abce2f1ccb46661f7f1c9470154
cristian-sanchez-dev <cristian.sanchez@protofy.xyz>
2023-10-09 02:10:19 +0000

    [FT](Web) Now the bundle page detects if a file is selected or not

--- packages/app/bundles/workspace/nextPages/workspace.tsx
+++ packages/app/bundles/workspace/nextPages/workspace.tsx
@@ -4,20 +4,34 @@ import PdfPreview from "../components/workspace/PdfPreviewer";
 import { WorkspaceProvider } from "../context/WorkspaceContext";
 import { useSession, withSession } from 'protolib'
 import { NextPageContext } from 'next'
+import { atom, useAtom } from 'jotai'
+
+export const FileBlobAtom = atom('')
 
 export function Workspace(props: any) {
   useSession(props.pageSession)
+  const [fileBlobUrl] = useAtom(FileBlobAtom)
 
-  return <div className="h-full w-full flex flex-row justify-start items-start
-    bg-red-400">
-    <div className="h-full min-w-[15vw] bg-[#171717] flex flex-col justify-start items-center
-    py-10">
-      <p className="text-red-400">PDF pages</p>
-      {/* <PdfPreview /> */}
-    </div>
-    {/*       <PdfPage />
-  <Editor /> */}
-  </div >
+  return (
+    <>
+      {
+        fileBlobUrl
+          ? <div className="h-full w-full flex flex-row justify-start items-start
+          bg-red-400">
+            <div className="h-full min-w-[15vw] bg-[#171717] flex flex-col justify-start items-center
+          py-10">
+              <p className="text-red-400">PDF pages</p>
+              {/* <PdfPreview /> */}
+            </div>
+            {/*       <PdfPage />
+        <Editor /> */}
+          </div >
+          : <div className="h-full w-full grid place-items-center">
+            <p>No file provided</p>
+          </div>
+      }
+    </>
+  )
 }
 
 export const serverExecuted = async (context: NextPageContext) => {
