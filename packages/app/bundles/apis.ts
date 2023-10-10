0b42ff9a7a26405da9b581c6b8a32036f45e3d52
lluis@protofy.xyz <lluis@protofy.xyz>
2023-10-10 05:31:24 +0200

    Add productsApi on apis in order to execute the autoapi

--- packages/app/bundles/apis.ts
+++ packages/app/bundles/apis.ts
@@ -1,5 +1,7 @@
 import {NotesAPI} from './notes/notesAPI'
+import { ProductsAPI } from './workspace/models/product/productsAPI'
 
 export default (app) => {
     NotesAPI(app)
+    ProductsAPI(app)
 }
