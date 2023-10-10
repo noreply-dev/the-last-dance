011b5a30c5141402eda20aea9394360a063e9f32
cristian-sanchez-dev <cristian.sanchez@protofy.xyz>
2023-10-10 02:24:47 +0000

    [FT](Parser)! Page upload is working now

--- apps/parser/src/routes/upload.js
+++ apps/parser/src/routes/upload.js
@@ -3,10 +3,11 @@ const path = require('path')
 const { Router } = require('express')
 const { _400, _500, _200 } = require('../lib/http')
 const uploadController = require('../controllers/upload.controller')
+const { PDFDocument } = require('pdf-lib')
 
 const router = Router()
 
-function resolvePdfUrl(req, res, next) {
+async function resolvePdfUrl(req, res, next) {
   const basePath = path.join(__dirname, '../../../../')
   const body = req.body
   console.log(body)
@@ -25,8 +26,9 @@ function resolvePdfUrl(req, res, next) {
     if (!pdfData) {
       throw new Error('Readed pdf data is empty')
     }
+    const file = await fileFromPdfPage(pdfData, body.targetPage)
 
-    fs.writeFileSync(tempPdfPath, pdfData)
+    fs.writeFileSync(tempPdfPath, file)
   } catch (err) {
     console.log(err)
     return _500(res, 'Cannot write file to the temp folder')
@@ -97,290 +99,28 @@ router.post('/page', async (req, res) => {
 
 module.exports = router
 
-const cachedResponses = [
-  {
-    products: [
-      {
-        name: 'FC-C110-1000',
-        price: 'Tramo recto en DN110/160 de 1000mm',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
+async function fileFromPdfPage(file, pageIndex) {
+  // initialize pdf-lib document and document slice 
+  const pdfDoc = await PDFDocument.load(file) // load
 
-        ]
-      },
-      {
-        name: 'FC-C110-2000',
-        price: 'Tramo recto en DN110/160 de 2000mm',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
+  // If file has only one page return the file
+  if (pdfDoc.getPageCount() <= 1) {
+    const pdfBytes = await pdfDoc.save()
 
-        ]
-      },
-      {
-        name: 'FC-CE110-15',
-        price: 'Codo de 15º en DN110/160',
-        color: '7 738 113 102',
-        'reference code': 'undefined',
-        extra_data: [
+    // convert to a file 
+    const blob = new Blob([pdfBytes], { type: 'application/pdf' })
+    const pageFile = new File([blob], `${Date.now()}.pdf`, { type: "application/pdf" })
 
-        ]
-      },
-      {
-        name: 'FC-CE110-30',
-        price: 'Codo de 30º en DN110/160',
-        color: '7 738 113103',
-        'reference code': '70',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'FC-CE110-45',
-        price: 'Codo de 45º en DN110/160',
-        color: '7 738 113 104',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'FC-CE110-87',
-        price: 'Codo de 87º en DN110/160',
-        color: '7 738 113 105',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'Adaptador para tejado plano DN166 con inclinación de 0-15º y 170mm',
-        price: 'FC-O110',
-        color: '7 738 113 127',
-        'reference code': 'con inclinación de 0-15º y 170mm',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'Teja negra de paso DN166 y cubiertas con inclinación de entre 5-25º',
-        price: '7 738 113 128',
-        color: 'con inclinación de entre 5-25º',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'Teja negra de paso DN166 y cubiertas con inclinación de entre 25-45º',
-        price: '7 738 113 129',
-        color: 'con inclinación de entre 25-45º',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'Adaptador de montaje',
-        price: 'paralelo',
-        color: 'DN110/185 a DN110 / DN110',
-        'reference code': '7 736 701 921',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'Nota: Sólo el kit FC-Set110-C33x incluye el deflector de salida de gases.',
-        price: 'undefined',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'Catálogo general',
-        price: 'undefined',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      }
-    ]
-  },
-  {
-    products: [
-      {
-        name: 'WJC215A0021001',
-        price: '6,2',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'WJC215A0022001',
-        price: '6,2',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'WJC215A0053001',
-        price: '12',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'WJC215A0021036',
-        price: '6,2',
-        color: 'INCH 2.4x2.4x4.7',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'WJC215A0022036',
-        price: '14,1,3,6',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'WJC215A0053036',
-        price: 'INCH 2.8x1.4x1.3,INCH 5.5x1.4x1.3',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'WJC215A0052001',
-        price: '6,2,8,7',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'WJC215A0004001',
-        price: '6,8,14,5',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'WJC215A0005001',
-        price: '12,14,6,8,1',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'WJC215A0052036',
-        price: '8,7',
-        color: 'INCH 2.4x3.4x5.7',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'WJC215A0004036',
-        price: 'undefined',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      },
-      {
-        name: 'WJC215A0005036',
-        price: 'undefined',
-        color: 'undefined',
-        'reference code': 'undefined',
-        extra_data: [
-
-        ]
-      }
-    ]
-  },
-  {
-    products: [
-      {
-        name: 'VÁLVULA DE DESAGÜE CLICK-CLACK*',
-        'reference code': [
-          'WJC205G0018001',
-          'WJC205G0018036'
-        ],
-        color: [
-          'undefined'
-        ],
-        price: [
-          '25,00 €',
-          '66,00 €'
-        ],
-        extra_data: [
-          '6,5',
-          '6,2'
-        ],
-        additional_properties: [
-          'Logic',
-          'Architect'
-        ]
-      }
-    ]
-  },
-  {
-    products: [
-      {
-        name: 'NEE 250C-250-3 ECU',
-        price: 'undefined',
-        color: 'undefined',
-        reference_code: 'undefined',
-        extra_data: [
-          'Tensión nominal de empleo: hasta 800V(AC)',
-          'Tipo de red: AC',
-          'Frecuencia de red: 50/60 Hz',
-          'Corriente nominal: hasta 600A',
-          'Tensión nominal de aislamiento [Ui]: 800V (AC)',
-          'Tecnología de disparo: termomagnético ajustables térmica y magnéticamente',
-          'Montaje en superficie'
-        ]
-      },
-      {
-        name: 'NEE 160C-160-4 TM',
-        price: '45',
-        color: 'undefined',
-        reference_code: 'undefined',
-        extra_data: [
-          'Tensión nominal de empleo: hasta 800V(AC)',
-          'Tipo de red: AC',
-          'Frecuencia de red: 50/60 Hz',
-          'Corriente nominal: hasta 600A',
-          'Tensión nominal de aislamiento [Ui]: 800V (AC)',
-          'Tecnología de disparo: termomagnético ajustables térmica y magnéticamente',
-          'Montaje en superficie'
-        ]
-      }
-    ]
+    return pageFile
   }
 
-]
+  const singlePagePdf = await PDFDocument.create() // create
+
+  // get the page and push the page
+  const [copiedPage] = await singlePagePdf.copyPages(pdfDoc, [pageIndex])
+  singlePagePdf.addPage(copiedPage)
+
+  // save pdf content
+  const pdfBytes = await singlePagePdf.save()
+  return pdfBytes
+}
